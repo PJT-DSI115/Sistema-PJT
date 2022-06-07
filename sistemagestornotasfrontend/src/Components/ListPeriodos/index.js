2942dee3ae7a5fea519d4633b392dc213e0ede5e
Jason Martinez <jason.guerra253@gmail.com>
2022-06-06 20:37:01 -0600

    Se agrego listado de periodos temporal para pruebas

---
+++ sistemagestornotasfrontend/src/Components/ListPeriodos/index.js
@@ -0,0 +1,19 @@
+
+import { Link, Outlet } from 'react-router-dom'
+import './index.css'
+function ListPeriodos() {
+
+    return (
+        <div>
+            <div className= "main__periodo">
+                <h3 className= "periodo__title">Periodos</h3>
+                <Link to = '1' className="periodo__link">P-2022</Link> 
+            </div>
+                <Outlet/>
+        </div>
+    );
+}
+
+export {
+    ListPeriodos
+}
